5e6ddd98fd5ac8c528684c1ab55030af130ba011
eliott0 <eliott@circles.asia>
2018-09-18 10:26:09 +0800

    Avant tentative form

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 	root 'pages#welcome'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  get '/bio' => 'bio#index'
+  get '/work' => 'work#index'
+  get '/contact' => 'contact#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
